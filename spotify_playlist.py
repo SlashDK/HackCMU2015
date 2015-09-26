de759d6600423a69054e7cc03987672fb61f4256
Taisuke Yasuda <yasuda.taisuke1@gmail.com>
2015-09-26 03:47:44 -0400

    Update spotify_playlist.py
    Extract songs from the artist

--- spotify_playlist.py
+++ spotify_playlist.py
@@ -2,4 +2,19 @@ import spotipy
 import pyen
 
 sp = spotipy.Spotify()
-en = pyen.Pyen(ZEUOKB8IFS86JL28M)
+en = pyen.Pyen('ZEUOKB8IFS86JL28M')
+
+name = 'Lady Gaga'
+
+response = en.get('playlist/static', artist=name, 
+	bucket=['id:spotify'])
+
+for song in response['songs']:
+	song = str(song)
+	song = song[1:-1]
+	info = song.split(",")
+	for entry in info:
+		if(entry.startswith(' u\'id\'')):
+			song_id = entry[10:-1]
+			print(song_id)
+	
