0c0e7b45c7bab4d58ea0cca34732ad5322bc20d5
SlashDK <dkhurana@andrew.cmu.edu>
2015-09-26 18:25:44 -0400

    Finished

--- app.py
+++ app.py
@@ -14,7 +14,20 @@ def index():
         # [...]
         artist=Main.main(form.book.data)
         print( artist)
-        return render_template('index.html', form=artist)
+        if(artist=="Hans Zimmer"):
+            album_uri = "spotify:album:61xHE9mlaQK7Pn02Qr3Scy";
+        elif(artist=="Steve Jablonsky"):
+            album_uri="spotify:album:1TA5Qdd6dgt9vGw9JZqJDF"
+        elif(artist=="Eluvium"):
+            album_uri="spotify:album:4BMYwbOZd1i9vraiSXtSkg"
+        elif(artist=="John Williams"):
+            album_uri="spotify:album:7n26GKaWGJ1sx9CXKISyyE"
+        elif(artist=="Kenny G"):
+            album_uri="spotify:album:5ie513eFL7o8i9kesVzoNW"
+        elif(artist=="Beethoven"):
+            album_uri="spotify:album:5zhKcnv4IvJXZ8oj3TyYCH"
+        print(album_uri)
+        return render_template('index.html', form=album_uri)
     #print(form.validate_on_submit())
     return render_template('index.html')
 
@@ -22,20 +35,23 @@ def index():
 def qwerty():
     return "Hello World"
 
-@app.route('/getspotify',methods=["GET"])
-def music():
-    print "music1"
-    art=request.args.get("q")
-    print "music2"
-    r = requests.get("http://ws.spotify.com/search/1/artist.json?q="+art)
-    print "music3"
-    return r
 
-@app.route('/getspotifyalbum',methods=["GET"])
-def music123():
-    artist_id=request.args.get("q")
-    r = requests.get('https://api.spotify.com/v1/artists/'+artist_id+'/albums')
-    return r
+
+
+# @app.route('/getspotify',methods=["GET"])
+# def music():
+#     print "music1"
+#     art=request.args.get("q")
+#     print "music2"
+#     r = requests.get("http://ws.spotify.com/search/1/artist.json?q="+art)
+#     print "music3"
+#     return r
+
+# @app.route('/getspotifyalbum',methods=["GET"])
+# def music123():
+#     artist_id=request.args.get("q")
+#     r = requests.get('https://api.spotify.com/v1/artists/'+artist_id+'/albums')
+#     return r
     
 
 
